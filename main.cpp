77f2823a9d98ad205005143a32a240074ac6ff84
ToxicTrigger <syoa1996@gmail.com>
2019-02-14 06:10:16 +0900

    :arrow_up: Fix WinMain

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@
 #endif
 
 
+#ifdef _WIN64
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
 {
     trigger::abst::renderer *renderer = new trigger::renderer::RENDERER(hInstance);  
 }
+#else
+int main()
+{
+    trigger::abst::renderer *renderer = new trigger::renderer::RENDERER(hInstance); 
+    return 0; 
+}
+#endif
